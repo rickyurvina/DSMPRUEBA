1d882a82420f483cd148f0186a7accca03cefc3e
rickyurvinaUCN <ricardo.urvina@alumnos.ucn.cl>
2023-11-21 08:31:20 -0300

    first commit

---
+++ app/src/main/java/com/example/evaluaciondsm/ui/home/HomeFragment.java
@@ -0,0 +1,87 @@
+package com.example.evaluaciondsm.ui.home;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.evaluaciondsm.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save();
+            }
+        });
+
+        binding.btnSearchP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                search();
+            }
+        });
+
+        binding.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                delete();
+            }
+        });
+
+        binding.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit();
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    //Metodo para guardar los productos
+    public void save() {
+
+    }
+
+    //Metodo para buscar productos
+    public void search() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void edit() {
+
+    }
+
+    public void cleanForm() {
+    }
+
+}
